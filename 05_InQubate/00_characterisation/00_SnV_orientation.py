45c17c25e4fbb76fc3c656270dd8f1b50a2e17a7
Sjoerd Loenen <sjoerdloenen@hotmail.com>
2026-02-02 16:47:42 +0100

    updated polar plot for defect orientation

--- 05_InQubate/00_characterisation/00_SnV_orientation.py
+++ 05_InQubate/00_characterisation/00_SnV_orientation.py
@@ -3,7 +3,8 @@ import matplotlib.pyplot as plt
 from matplotlib.widgets import Slider
 
 # Angular domain
-theta_SnV_rad = np.deg2rad( 1./2 * (180-109.5) )  # Fixed angle for SnV centers
+theta_snv_1_rad = np.deg2rad( 1./2 * (180-109.5) )  # Fixed angle relative to the waveguide direction for SnV centers aligned parallel to the direction of the waveguide
+theta_snv_3_4_rad = np.deg2rad( 90 )  # Fixed angle relative to the waveguide direction for SnV centers aligned perpendicular to the direction of the waveguide
 theta = np.linspace(0, 2*np.pi, 1000)
 
 # Dipole model
@@ -11,13 +12,16 @@ def dipole_excitation(theta, theta0):
     return np.cos(theta - theta0)**2
 
 # Initial parameters
-theta_snv_1 = 0           # variable dipole orientation
-theta_snv_3_4 = np.pi/2           # fixed dipole orientation
+theta_sample_init_deg = 25                              # initial sample angle in degrees
+theta_sample_init = np.deg2rad(theta_sample_init_deg)   # initial sample angle
 
 # Initial curves
-I_snv_1 = dipole_excitation(theta, theta_snv_1)
-I_snv_2 = dipole_excitation(theta, -theta_snv_1)
-I_snv_3_4 = dipole_excitation(theta, theta_snv_3_4)
+snv_1_polar_rad = np.arctan( np.sin(theta_sample_init) * np.tan(theta_snv_1_rad))
+snv_3_4_polar_rad = np.arctan( np.sin(theta_sample_init) * np.tan(theta_snv_3_4_rad))
+
+I_snv_1 = dipole_excitation(theta, snv_1_polar_rad)
+I_snv_2 = dipole_excitation(theta, -snv_1_polar_rad)
+I_snv_3_4 = dipole_excitation(theta, snv_3_4_polar_rad)
 
 # Create figure
 fig = plt.figure(figsize=(6, 6))
@@ -38,14 +42,14 @@ theta_sample_slider = Slider(
     label=r'$\theta_{sample}$ (deg)',
     valmin=0,
     valmax=90,
-    valinit=theta_snv_1*180/np.pi
+    valinit=theta_sample_init_deg
 )
 
 # Update function
 def update(val):
     theta_sample_deg = theta_sample_slider.val
     theta_sample_rad = np.deg2rad(theta_sample_deg)
-    theta_polar_rad = np.arctan( np.sin(theta_sample_rad) * np.tan(theta_SnV_rad))
+    theta_polar_rad = np.arctan( np.sin(theta_sample_rad) * np.tan(theta_snv_1_rad))
     line1.set_ydata(dipole_excitation(theta, theta_polar_rad))
     line2.set_ydata(dipole_excitation(theta, -theta_polar_rad))
     fig.canvas.draw_idle()
